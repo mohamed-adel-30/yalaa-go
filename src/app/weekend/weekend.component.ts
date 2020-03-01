55fe55e290f82d8b56deaea2146f1b4ce990a6f2
Mohamed Adel <madel910@outlook.com>
2020-03-01 15:40:12 +0200

    fonts & colors

--- src/app/weekend/weekend.component.ts
+++ src/app/weekend/weekend.component.ts
@@ -5,6 +5,7 @@ import { UsersService } from '../users.service';
 
 
 import * as $ from 'jquery';
+import { log } from 'util';
 
 
 @Component({
@@ -13,66 +14,145 @@ import * as $ from 'jquery';
   styleUrls: ['./weekend.component.scss']
 })
 export class WeekendComponent implements OnInit {
+
+  //submitForm
   holidayForm: FormGroup;
   userObj;
-  user=[];
-  weekEnd={}
-  constructor(private formBuilder: FormBuilder , private http:HttpServiceService , private service:UsersService ){ }
+  user = [];
+  weekEnd = {}
+  plans;
+  locations;
+  placeLocation: any = [];
+  returnedPlans = [];
+  filteredPlans;
+
+  // planForm
+  planForm: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder, private http: HttpServiceService, private service: UsersService) { }
 
   ngOnInit() {
+
+    //submitForm
     this.holidayForm = this.formBuilder.group({
-      time: ['', Validators.required],
-      endTime: ['', Validators.required],
+      // time: ['', Validators.required],
+      // endTime: ['', Validators.required],
 
       date: ['', Validators.required],
-      email: ['', [ Validators.pattern(/^[a-z]\w{1,}@[a-z]{1,}.com$/)]],
+      email: ['', [Validators.pattern(/^[a-z]\w{1,}@[a-z]{1,}.com$/)]],
 
     })
-    
-    
-    this.userObj=this.http.getData("user")
+
+    //////
+    this.http.gettingPlaces().subscribe(data => {
+      this.plans = data;
+      console.log(this.plans);
+      for (let singlePlace of this.plans) {
+        this.placeLocation.push(singlePlace.location)
+      }
+
+      let filteredPlaces = new Set(this.placeLocation);
+      this.locations = [...filteredPlaces];
+    })
+
+    //////
+    this.userObj = this.http.getData("user")
     console.log(this.userObj);
+
+    this.user.push(this.userObj)
+
+    $('#notSend').click(function () {
+      $('.email').css("display", "block")
+    })
+    $('#send').click(function () {
+      $('.email').css("display", "none")
+    })
+
+    // $('.show').click(function () {
+    //   $('.plan').css('display', 'block')
+
+    // })
+    // planForm
+    this.planForm = this.formBuilder.group({
+      location: ['', Validators.required],
+      haveChild: ['', Validators.required],
+      time: ['', Validators.required],
+      endTime: ['', Validators.required]
+    })
+
+    $('.userName').click(function () {
+      alert('clicked');
+    })
+
+
+  }
+  // onSubmit(form) {
+  //   if (form.value.email == "") {
+  //     this.weekEnd = {
+  //       "userId": this.userObj.id,
+  //       "date": form.value.date,
+  //       "time": form.value.time,
+  //       "endTime": form.value.endTime,
+  //       "email": this.userObj.email
+  //     }
+
+
+  //     this.service.addWeekend(this.weekEnd)
+  //   }
+  //   else {
+  //     this.weekEnd = {
+  //       "userId": this.userObj.id,
+  //       "date": form.value.date,
+  //       "time": form.value.time,
+  //       "endTime": form.value.endTime,
+  //       "email": form.value.email
+  //     }
+
+  //     this.service.addWeekend(this.weekEnd)
+  //   }
+  //   console.log(this.weekEnd);
+
+  // }
+
+
+
+  onOk(form) {
+
+
+
+    this.returnedPlans = [];
+
+    for (let plan of this.plans) {
+      if (plan.location == form.value.location && plan["kid-friendly"] == form.value.haveChild) {
+        this.returnedPlans.push(plan);
+      }
+    }
+    // console.log(this.plans);
     
-   this.user.push(this.userObj)
-   console.log(this.user);
-   
-   $('#notSend').click(function(){
-     $('.email').css("display","block")
-   })
-   $('#send').click(function(){
-    $('.email').css("display","none")
-  })
+    this.filteredPlans = this.returnedPlans.map(() => this.returnedPlans.splice(0, 3)).filter(returnedPlans => returnedPlans)
+    console.log(this.filteredPlans);
 
-  $('.userName').click(function(){
-    alert('clicked');
-  })
-
-
-  }
-  onSubmit(form){
-    if(form.value.email== ""){
-    this.weekEnd={
-      "userId": this.userObj.id,
-      "date": form.value.date,
-      "time": form.value.time,
-      "endTime": form.value.endTime,
-      "email":this.userObj.email
+    for (let index = 0; index < this.filteredPlans.length; index++) {
+      this.filteredPlans[index].prop = index;      
+      
     }
 
-    this.service.addWeekend(this.weekEnd)
   }
-  else{
-    this.weekEnd={
-      "userId": this.userObj.id,
-      "date": form.value.date,
-      "time": form.value.time,
-      "endTime": form.value.endTime,
-      "email":form.value.email
-    }
 
-    this.service.addWeekend(this.weekEnd)
-  }
+
+
+
+  book(location) {
+    console.log("mohamed ahbal");
+    this.weekEnd = {
+      "userId": this.userObj.id,
+      "email": this.userObj.email,
+      "location": location.id
+    }
     console.log(this.weekEnd);
-    
+
+    this.service.addWeekend(this.weekEnd);
   }
+
 }
